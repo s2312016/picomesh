ad1e8472f4db7b4da6032e8ba621b51404b71cfc
中山 雄斗 <s2312016@s.do-johodai.ac.jp>
2026-07-11 10:57:19 +0900

    Add Arduino umbrella header

---
+++ src/PicoMesh.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "picomesh/checksum.h"
+#include "picomesh/frame.h"
+#include "picomesh/node_registry.h"
+#include "picomesh/reliability.h"
+#include "picomesh/sequence_tracker.h"
+#include "picomesh/stream_decoder.h"
+#include "picomesh/transport.h"
+
+#ifdef ARDUINO
+#include "arduino/wire_transport.h"
+#endif
