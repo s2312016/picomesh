b26aa8bcdba5988f1e0d0a019f89b0abf371cf61
中山 雄斗 <s2312016@s.do-johodai.ac.jp>
2026-07-11 23:26:12 +0900

    Document version constants

--- include/picomesh/version.h
+++ include/picomesh/version.h
@@ -1,15 +1,23 @@
 #pragma once
 
+/** PicoMesh semantic-version major component. */
 #define PICOMESH_VERSION_MAJOR 0
+/** PicoMesh semantic-version minor component. */
 #define PICOMESH_VERSION_MINOR 1
+/** PicoMesh semantic-version patch component. */
 #define PICOMESH_VERSION_PATCH 0
+/** PicoMesh semantic version as a string literal. */
 #define PICOMESH_VERSION_STRING "0.1.0"
 
 namespace picomesh {
 
+/** Compile-time semantic-version major component. */
 inline constexpr unsigned version_major = PICOMESH_VERSION_MAJOR;
+/** Compile-time semantic-version minor component. */
 inline constexpr unsigned version_minor = PICOMESH_VERSION_MINOR;
+/** Compile-time semantic-version patch component. */
 inline constexpr unsigned version_patch = PICOMESH_VERSION_PATCH;
+/** Compile-time semantic version string. */
 inline constexpr const char* version_string = PICOMESH_VERSION_STRING;
 
 }  // namespace picomesh
